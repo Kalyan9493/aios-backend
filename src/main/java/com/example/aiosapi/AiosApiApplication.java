c353f19fea72387c89046264bda4d69bd89e6f0f
Kalyan <kalyankumarreddy098@gmail.com>
2022-01-02 21:06:03 +0530

    Intial setup is done

---
+++ src/main/java/com/example/aiosapi/AiosApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.aiosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiosApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiosApiApplication.class, args);
+    }
+
+}
